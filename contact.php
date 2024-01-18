7b033d863e5384293db3d2f8756aae93a179fbe0
optr1 <33418793+optr1@users.noreply.github.com>
2024-01-18 09:09:17 +0300

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,265 @@
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+  <link rel="stylesheet" href="css/bootstrap.css">
+  <link rel="stylesheet" href="style1.css">
+  <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+  <title> تواصل معنا </title>
+</head>
+
+<body>
+  <header class="header">
+
+    <nav class="navbar navbar-expand-lg navbar-light ">
+      <div class="container">
+
+        <div class="logo-navbar" >
+          <a  href=""><img src="images/BEEAH-Logo-PNG (1).png" class="logo-nav " alt=""></a>
+        </div>
+
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarNav">
+
+          <ul class="navbar-nav ms-auto">
+
+            <li >
+              <a href="home2.html">الرئيسية</a>
+            </li>
+
+            <li class="nav-item dropdown">
+              <a class="nav-link about-nav-a  " href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" >
+                نبذه عنا
+              </a>
+              <ul class="dropdown-menu " >
+                <li><a class="dropdown-item2" href="#">م.علي الشعيبي</a></li>
+                <li><a class="dropdown-item2" href="#">فريقنا</a></li>
+              </ul>
+            </li>
+
+            <li>
+              <a href="services.html">خدماتنا</a>
+          </li>
+                          
+          <li>
+            <a href="project4.html">المشاريع</a>
+            </li>
+
+            <li>
+              <a href="#">أفكار</a>
+          </li>
+
+          <li><a href="#">التوظيف</a></li>
+         
+          <li>
+            <a class="contact-nav-a" href="contact.html">تواصل معنا</a>
+        </li>
+
+              
+          <li><a href="#contact">English</a></li>
+           
+
+
+
+
+          </ul>
+        </div>
+
+      </div>
+    </nav>
+  </header>
+  <br><br>
+  <section class="bg-t-our">
+    <div class="t-our-1">
+      <a href="">تواصل معنا</a>
+      <a href="#home">الرئيسية</a>
+    </div>
+  </section>
+
+
+  <br><br><br><br><br><br><br><br><br>
+
+
+  <div>
+    <h1 class="contact-h1-page">تواصل معنا</h1>
+  </div>
+
+  <br><br><br>
+
+  <!--Google map-->
+  <section class="form-map-contact-page">
+    <div class="map-contact-page">
+      <iframe
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3622.757096054713!2d46.69116662475587!3d24.76951617798873!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3e2efd3fc1c49ca5%3A0xc87da5bcea94f384!2z2YXZg9iq2Kgg2Ygg2LTYsdmD2Kkg2KfZhNio2YrYptipINmF2K7Yt9i32YjZhiDZhdi52YXYp9ix2YrZiNmGINmI2YXZh9mG2K_Ys9mI2YY!5e0!3m2!1sar!2ssa!4v1698221963061!5m2!1sar!2ssa"
+        width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"
+        referrerpolicy="no-referrer-when-downgrade"></iframe>
+    </div>
+
+
+
+    <!-- contact form -->
+
+<form class="display-grid row-gap-1-rem" method="post">
+    <input class="box-shadow-primary" name="name" type="text" placeholder="Name" autocomplete="off" required />
+    <input class="box-shadow-primary" name="email" type="email" placeholder="Email" autocomplete="off" required />
+    <input class="box-shadow-primary" name="subject" type="text" placeholder="Subject" autocomplete="off" required />
+    <textarea class="box-shadow-primary" name="message" placeholder="Message..." required></textarea>
+    <button type="submit" name="send">
+      Send <i class="fa-solid fa-paper-plane color-white margin-left-1-rem"></i>
+    </button>
+  </form>
+  <br><br>
+
+
+
+
+  <footer class="text-center text-lg-start  text-muted" style="background-color: #050505;">
+    <div class="container">
+      <section class="d-flex justify-content-center justify-content-lg-between p-4 ">
+        <!-- Right -->
+        <div>
+          <a href="" class="me-4 text-reset icon">
+            <img src="images/icons8-facebook-24.png" style="width:20px; height:20px;">
+          </a>
+          <a href="" class="me-4 text-reset">
+            <img src="images/behance.png" style="width:20px; height:20px;">
+          </a>
+          <a href="" class="me-4 text-reset icon">
+            <img src="images/icons8-instagram-32.png" style="width:20px; height:20px;">
+          </a>
+        </div>
+        <!-- Left -->
+
+        <div class="me-5 d-none d-lg-block">
+          <a href="" class="me-4 text-reset">
+            <img src="images/dark_logo-150x150.png" style="width: 150px; height:150px" alt="">
+          </a>
+        </div>
+
+        <!-- Right -->
+      </section>
+      <!-- Section: Social media -->
+
+      <!-- Section: Links  -->
+      <section id="contact">
+        <div class="container text-center text-md-start mt-5">
+          <!-- Grid row -->
+          <div class="row mt-3">
+            <!-- Grid column -->
+            <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+              <h6 class="text-uppercase fw-bold mb-4"><button style="background-color:#c6b37e;color: #ffffff;">
+                  <img src="images/direct-download.png" class="m-1" style="width: 15px; height:15px; " alt="">تحميل
+                  الملف التعريفي</button>
+              </h6>
+
+
+              <nav class="navigations-footer text-end" style="color: #ffffff;">
+
+                <a href="#home" class="text-reset link-custom">الرئيسية</a>
+                <a href="#aboutus" class="text-reset link-custom">نبذة عنا</a>
+                <a href="#services" class="text-reset link-custom">خدماتنا</a>
+                <a href="#projects" class="text-reset link-custom">المشاريع</a>
+                <a href="#insights" class="text-reset link-custom">أفكار</a>
+                <a href="#career" class="text-reset link-custom">التوظيف</a>
+                <a href="#contact" class="text-reset link-custom">تواصل معنا</a>
+                <a href="#contact">English</a>
+
+              </nav>
+            </div>
+
+            <!-- Grid column -->
+
+            <!-- Grid column -->
+            <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+              <!-- Links -->
+              <h6 class="text-uppercase fw-bold mb-4" style="color: #ffffff;">
+                تواصل معنا
+              </h6>
+              <div style="color: #ffffff; opacity: 60%;">
+                <p class="text-reset">البيئة: مخططون ومعماريون ومهندسون </p>
+              </div>
+
+              <div style="color: #ffffff; opacity: 60%;">
+                <p class="text-reset address">ص.ب.85745,الرياض,11612 المملكة العربية السعودية</p>
+              </div>
+
+              <div style="color: #ffffff; opacity: 60%;">
+                <p class="text-reset phone">التلفون:
+                  <a class="" href="00966 11 4543330" style="color: #ffffff; opacity: 60%;">00966 11 4543330</a>
+                </p>
+              </div>
+
+              <div style="color: #ffffff; opacity: 60%;">
+                <p class="text-reset phone">فاكس:
+                  <a class="phone" href="00966 11 4550664" style="color: #ffffff; opacity: 60%;">00966 11 4550664</a>
+                </p>
+              </div>
+            </div>
+
+
+            <!-- Grid column -->
+
+            <!-- Links -->
+
+
+            <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4 Rights">
+              <!-- Content -->
+              <h6 class="text-uppercase text-end fw-bold mb-4" style="color: #ffffff; ">
+                سجل لتبقى على اطلاع
+              </h6>
+              <p class="text-end" style="color: #ffffff; opacity: 60%;">
+                اشترك في النشرة الإخبارية لتكون على اطلاع.
+              </p>
+              <br> <br>
+
+              <div> <input type="email" style="color: #ffffff; opacity: 60%;" class="form-control"
+                  placeholder="ادخل بريدك الإلكتروني"> </div>
+              <br>
+              <button class="button-email btn btn-secondary" style="color: #ffffff; opacity: 30%;"></button>
+
+            </div>
+
+          </div>
+          <!-- Grid column -->
+        </div>
+        <!-- Grid row -->
+    </div>
+    </section>
+    <!-- Section: Links  -->
+    <hr class="line">
+    <section>
+      <div class="text-end p-4 Rights" style="color: #ffffff; opacity: 60%;">
+
+        © 2022البيئة,كافة الحقوق محفوظة.
+
+      </div>
+    </section>
+
+
+    </div>
+  </footer>
+
+  <!-- /FOOTER      -->
+
+
+
+
+
+  <script src="js/popper.min.js"></script>
+  <script src="js/jquery-3.7.1.min.js"></script>
+  <script src="js/bootstrap.js"></script>
+
+
+</body>
+
+</html>
